5989be62bcbd4194acd4b8baa63b96a857cdc5d8
gAlex71 <alexvladimirovich71@mail.ru>
2023-08-12 09:48:42 +0300

    media ready

--- backend-hosting/src/media/media.controller.ts
+++ backend-hosting/src/media/media.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Query,
+  UploadedFile,
+  HttpCode,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
 import { MediaService } from './media.service';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('media')
 export class MediaController {
   constructor(private readonly mediaService: MediaService) {}
+
+  @HttpCode(200)
+  @Post()
+  @Auth()
+  //Модуль требуется для загрузки файлов
+  @UseInterceptors(FileInterceptor('media'))
+  async uploadMediaFile(
+    @UploadedFile() mediaFile: Express.Multer.File,
+    @Query('folder') folder?: string,
+  ) {
+    return this.mediaService.saveMedia(mediaFile, folder);
+  }
 }
--- backend-hosting/src/media/media.module.ts
+++ backend-hosting/src/media/media.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MediaService } from './media.service';
 import { MediaController } from './media.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { path } from 'app-root-path';
 
 @Module({
+  imports: [
+    //Делаем корневую папку статичной
+    ServeStaticModule.forRoot({
+      rootPath: `${path}/uploads`,
+      serveRoot: '/uploads'
+    })
+  ],
   controllers: [MediaController],
   providers: [MediaService]
 })
