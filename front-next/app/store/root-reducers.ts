70df47b2a1bf8235d8e0c6b1a84b338bb0392424
gAlex71 <alexvladimirovich71@mail.ru>
2023-08-20 12:57:58 +0300

    auth store

---
+++ front-next/app/store/root-reducers.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import { authSlice } from "./auth/auth.slice";
+import { reducer as toastrReducer } from 'react-redux-toastr';
+
+export const rootReducer = combineReducers({
+    auth: authSlice.reducer,
+    //Обязательно указываем последним!
+    toastr: toastrReducer
+})
